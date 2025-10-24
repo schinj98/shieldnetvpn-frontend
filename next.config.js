fda02e40a6304a94ea445e33848b61edf96214a7
Sachin Jangid <sachinreal2003@gmail.com>
2025-10-24 19:23:52 +0530

    added axxios2

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    appDir: true, // App Router enable
+  },
   env: {
     NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080',
   },
+  output: 'standalone', // optional, optimized production
 }
-
 module.exports = nextConfig
